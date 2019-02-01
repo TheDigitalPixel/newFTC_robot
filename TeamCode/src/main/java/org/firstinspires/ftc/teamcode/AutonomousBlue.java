39e9946b277817fa3a78d6418f739d9206736c5c
TheDigitalPixel <jacob.bass01@gmail.com>
2019-02-01 17:54:25 -0500

    Update AutonomousBlue.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousBlue.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
